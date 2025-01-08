5dbad3809691d759abfac956789a73ae6cb55e7d
I-M-Marinov <lcfrrr@gmail.com>
2025-01-08 11:39:03 +0200

    Exercise 04. Circle Area

--- Syntax, Functions and Statements - Lab/exercises.js
+++ Syntax, Functions and Statements - Lab/exercises.js
@@ -45,3 +45,18 @@ function solve(num1, num2, num3) {
     }
     console.log(result);
 }
+
+// 04. Circle Area
+
+function solve(input) {
+    let result;
+
+    let inputType = typeof(input);
+
+    if(inputType === 'number'){
+        result = Math.pow(input,2)*Math.PI;
+        console.log(result.toFixed(2));
+    } else {
+        console.log(`We can not calculate the circle area, because we receive a ${inputType}.`)
+    }
+}

c5588c690622bd48eea9001a053047fd43145353
I-M-Marinov <lcfrrr@gmail.com>
2025-01-08 11:21:17 +0200

    Exercise 2.	String Length

--- Syntax, Functions and Statements - Lab/exercises.js
+++ Syntax, Functions and Statements - Lab/exercises.js
@@ -7,3 +7,20 @@ function echo(inputString){
 
 echo("Hello, SoftUni!");
 
+// 2. String Length
+
+function solve(arr1, arr2, arr3){
+    let sumLength; 
+    let averageLength;
+
+    let firstArgumentLength = arr1.length;
+    let secondArgumentLength = arr2.length;
+    let thirdArgumentLength = arr3.length;
+
+    sumLength = firstArgumentLength + secondArgumentLength + thirdArgumentLength;
+    averageLength = Math.floor(sumLength / 3 );
+
+    console.log(sumLength);
+    console.log(averageLength);
+
+}

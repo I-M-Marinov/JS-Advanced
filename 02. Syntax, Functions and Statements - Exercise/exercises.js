5abd237b19e23b6f2ca7e3c06f237945abb6f00b
I-M-Marinov <lcfrrr@gmail.com>
2025-01-09 19:47:07 +0200

    Exercise 03. Same Numbers

--- 02. Syntax, Functions and Statements - Exercise/exercises.js
+++ 02. Syntax, Functions and Statements - Exercise/exercises.js
@@ -27,3 +27,31 @@ function gcd(a, b) {
 
   gcd(15,5);
   gcd(2154, 458);
+
+  // 03. Same Numbers
+
+// Write a function that takes an integer number as an input and checks if all the digits in a given number are the same or not.
+// Print on the console true if all numbers are the same and false if not. On the next line, print the sum of all digits.
+// The input comes as an integer number.
+// The output should be printed on the console.
+
+function checkDigits(number) {
+
+    const digits = number.toString().split("").map(Number); 
+    let allSame = true;
+    let sum = 0;
+  
+    for (let i = 0; i < digits.length; i++) {
+      if (digits[i] !== digits[0]) {  
+        allSame = false;  
+      }
+      sum += digits[i]; 
+    }
+  
+    console.log(allSame); 
+    console.log(sum); 
+}
+  
+  checkDigits(2222222); 
+  checkDigits(1234); 
+  

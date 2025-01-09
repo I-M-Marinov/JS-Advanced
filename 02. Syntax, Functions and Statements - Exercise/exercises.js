e0a236203f509851783c0f27d873f32e015d3385
I-M-Marinov <lcfrrr@gmail.com>
2025-01-09 18:46:34 +0200

    Exercise 01. Fruit

---
+++ 02. Syntax, Functions and Statements - Exercise/exercises.js
@@ -0,0 +1,12 @@
+// 01. Fruit
+
+function calculate(fruit, weight, pricePerKg){
+
+    let weightInKg = weight / 1000;
+    let price = weightInKg * pricePerKg;
+    
+    console.log(`I need $${price.toFixed(2)} to buy ${weightInKg.toFixed(2)} kilograms ${fruit}.`)
+}
+
+calculate('orange', 2500, 1.80);
+calculate('apple', 1563, 2.35);

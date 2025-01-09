ee65ec6e8b3d9f5d1c95cdf833466ed237593710
I-M-Marinov <lcfrrr@gmail.com>
2025-01-09 20:06:39 +0200

    Exercise 04. Previous Day

--- 02. Syntax, Functions and Statements - Exercise/exercises.js
+++ 02. Syntax, Functions and Statements - Exercise/exercises.js
@@ -54,4 +54,26 @@ function checkDigits(number) {
   
   checkDigits(2222222); 
   checkDigits(1234); 
-  
+
+  // 04. Previous Day
+
+// Write a JS function that calculates the date of the previous day by a given year, month, and day.
+// The input comes as three numeric parameters. The first element is the year, the second is the month and the third is the day.
+// The output must be the return date of the previous day in the format: `{year}-{month}-{day}`
+
+  function getPreviousDay(year,month,day){
+
+    const date = new Date(year, month - 1, day); 
+    date.setDate(date.getDate() - 1);
+
+    let newYear = date.getFullYear();
+    let newMonth = date.getMonth();
+    let newDay = date.getDate();
+
+    console.log(`${newYear}-${newMonth + 1}-${newDay}`);
+    
+  }
+
+  getPreviousDay(2016, 9, 30);
+  getPreviousDay(2015, 5, 10);
+  getPreviousDay(2015, 1, 1);

2c9ca74e15532abd3eb0e9bcfebedfcf58210f7f
I-M-Marinov <lcfrrr@gmail.com>
2025-01-09 18:53:57 +0200

    Exercise 02. Greatest Common Divisor – GCD

--- 02. Syntax, Functions and Statements - Exercise/exercises.js
+++ 02. Syntax, Functions and Statements - Exercise/exercises.js
@@ -10,3 +10,20 @@ function calculate(fruit, weight, pricePerKg){
 
 calculate('orange', 2500, 1.80);
 calculate('apple', 1563, 2.35);
+
+// 02. Greatest Common Divisor – GCD
+
+// Write a function that takes two positive numbers as input and computes the greatest common divisor.	
+// The input comes as two positive integer numbers.
+// The output should be printed on the console.
+
+function gcd(a, b) {
+    if (b === 0) {
+      console.log(a);
+      return;
+    }
+    return gcd(b, a % b);
+  }
+
+  gcd(15,5);
+  gcd(2154, 458);

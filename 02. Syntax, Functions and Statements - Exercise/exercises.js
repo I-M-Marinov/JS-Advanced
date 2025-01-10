096d5604411d7b4fee76dac72e80977a4763ef01
I-M-Marinov <lcfrrr@gmail.com>
2025-01-10 19:13:03 +0200

    Exercise 06. Road Radar

--- 02. Syntax, Functions and Statements - Exercise/exercises.js
+++ 02. Syntax, Functions and Statements - Exercise/exercises.js
@@ -111,3 +111,55 @@ function calculateTime(steps, lengthOfFootprint, speedInKmH) {
 
 calculateTime(4000, 0.60, 5);
 calculateTime(2564, 0.70, 5.5);
+
+// 06. Road Radar
+
+// Write a function that determines whether a driver is within the speed limit. You will receive the speed and the area. Each area has a different limit: 
+// •	On the motorway, the limit is 130 km/h
+// •	On the interstate, the limit is 90 km/h
+// •	In the city, the limit is 50 km/h 
+// •	Within a residential area, the limit is 20 km/h
+// If the driver is within the limits, there should be a printed speed and the speed limit. 
+//                 `Driving {speed} km/h in a {speed limit} zone`
+// If the driver is over the limit, however, your function should print the severity of the infraction and the difference in speeds. 
+// `The speed is {difference} km/h faster than the allowed speed of {speed limit} - {status}`
+// For speeding up to 20 km/h over the limit, the status should be speeding.
+// For speeding up to 40 km/h over the limit, the status should be excessive speeding.
+// For anything else, status should be reckless driving.
+// The input comes as 2 string parameters. The first element is the current speed (number), the second element is the area.
+
+
+function checkSpeed(speed, area) {
+
+  const limits = {
+      motorway: 130,
+      interstate: 90,
+      city: 50,
+      residential: 20,
+  };
+
+  const speedLimit = limits[area];
+  const difference = speed - speedLimit;
+
+  if (difference <= 0) {
+      console.log(`Driving ${speed} km/h in a ${speedLimit} zone`);
+  } else {
+      let status;
+      if (difference <= 20) {
+          status = 'speeding';
+      } else if (difference <= 40) {
+          status = 'excessive speeding';
+      } else {
+          status = 'reckless driving';
+      }
+
+      console.log(
+          `The speed is ${difference} km/h faster than the allowed speed of ${speedLimit} - ${status}`
+      );
+  }
+}
+
+checkSpeed(40, 'city'); 
+checkSpeed(21, 'residential');      
+checkSpeed(120, 'interstate'); 
+checkSpeed(200, 'motorway'); 

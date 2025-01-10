787b51cf293012f279c785efb9128db75e719674
I-M-Marinov <lcfrrr@gmail.com>
2025-01-10 22:37:55 +0200

    Exercise 08. Validity Checker

--- 02. Syntax, Functions and Statements - Exercise/exercises.js
+++ 02. Syntax, Functions and Statements - Exercise/exercises.js
@@ -225,3 +225,45 @@ function performOperations(input, op1, op2, op3, op4, op5) {
 
 performOperations('32', 'chop', 'chop', 'chop', 'chop', 'chop');
 performOperations('9', 'dice', 'spice', 'chop', 'bake', 'fillet');
+
+// 08. Validity Checker
+
+// Write a program that receives a total of 4 parameters in the format x1, y1, x2, y2. 
+// Check if the distance between each point (x, y) and the beginning of the Cartesian coordinate system (0, 0) is valid. 
+// A distance between two points is considered valid if it is an integer value. 
+
+// Note: You can use the following formula to help you calculate the distance between the points (x1, y1) and (x2, y2).
+// The order of comparisons should always be first {x1, y1} to {0, 0}, then {x2, y2} to {0, 0} and finally {x1, y1} to {x2, y2}. 
+// In case a distance is valid, print: `{x1, y1} to {x2, y2} is valid`
+// If the distance is invalid, print: `{x1, y1} to {x2, y2} is invalid`
+// The input consists of two points given as 4 numbers.
+// For each comparison print either `{x1, y1} to {x2, y2} is valid` if the distance is valid, or `{x1, y1} to {x2, y2} is invalid` if it is invalid.
+
+function checkDistances(x1, y1, x2, y2) {
+  
+  function isValidDistance(x1, y1, x2, y2) {
+    const distance = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+    return Number.isInteger(distance);
+  }
+
+  if (isValidDistance(x1, y1, 0, 0)) {
+    console.log(`{${x1}, ${y1}} to {0, 0} is valid`);
+  } else {
+    console.log(`{${x1}, ${y1}} to {0, 0} is invalid`);
+  }
+
+  if (isValidDistance(x2, y2, 0, 0)) {
+    console.log(`{${x2}, ${y2}} to {0, 0} is valid`);
+  } else {
+    console.log(`{${x2}, ${y2}} to {0, 0} is invalid`);
+  }
+
+  if (isValidDistance(x1, y1, x2, y2)) {
+    console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is valid`);
+  } else {
+    console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is invalid`);
+  }
+}
+
+checkDistances(3, 0, 0, 4);
+checkDistances(2, 1, 1, 1);

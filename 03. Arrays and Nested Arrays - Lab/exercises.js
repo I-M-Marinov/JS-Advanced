72233ac2cc9a916d095f3b6010d40fd88f276f10
Ivan Marinov <134225981+I-M-Marinov@users.noreply.github.com>
2025-01-11 12:00:31 +0200

    Exercise 02. Last K Numbers Sequence

--- 03. Arrays and Nested Arrays - Lab/exercises.js
+++ 03. Arrays and Nested Arrays - Lab/exercises.js
@@ -18,3 +18,29 @@ function evenPositionElements(input) {
 
 console.log(evenPositionElements(['20', '30', '40', '50', '60']));
 console.log(evenPositionElements(['5', '10']));
+
+// 02. Last K Numbers Sequence
+
+// You are given two integers n and k. Write a JS function that generates and return the following sequence:
+// •	The first element is 1
+// •	Every following element equals the sum of the previous k elements
+// •	The length of the sequence is n elements
+// The input comes as two number arguments. The first element represents the number n, and the second – the number k.
+// The output is the return value of your function and should be an array of numbers.
+
+function numbersSequence(n, k) {
+    let result = [1]; 
+
+    for (let i = 1; i < n; i++) {
+
+        let startIndex = Math.max(0, i - k); 
+        let sum = result.slice(startIndex, i).reduce((a, b) => a + b, 0);
+
+        result.push(sum); 
+    }
+
+    return result; 
+}
+
+console.log(numbersSequence(6, 3)); 
+console.log(numbersSequence(8, 2)); 

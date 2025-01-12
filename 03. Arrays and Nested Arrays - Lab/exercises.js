e617af461d71511ed9dfe5fa4ea71451d92e28e7
I-M-Marinov <lcfrrr@gmail.com>
2025-01-12 21:30:28 +0200

    Exercise 09. Biggest Element

--- 03. Arrays and Nested Arrays - Lab/exercises.js
+++ 03. Arrays and Nested Arrays - Lab/exercises.js
@@ -202,3 +202,19 @@ function oddPositions(array){
 console.log(oddPositions([10, 15, 20, 25]));
 console.log(oddPositions([3, 0, 10, 4, 7, 3]));
 
+
+// 09. Biggest Element
+
+// Write a function that finds the biggest element inside a matrix.
+// The input comes as an array of arrays, containing number elements (2D matrix of numbers).
+// The output is the return value of your function. Find the biggest element and return it.
+
+function biggestElement(matrix) {
+
+    const flattened = matrix.flat();
+    const biggest = Math.max(...flattened);
+    return biggest;
+  }
+
+console.log(biggestElement([[20, 50, 10],[8, 33, 145]]));
+console.log(biggestElement([[3, 5, 7, 12],[-1, 4, 33, 2],[8, 3, 0, 4]]));

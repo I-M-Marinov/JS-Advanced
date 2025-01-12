4efbd3f0423a53b3b572fed282afa07aa56175e5
I-M-Marinov <lcfrrr@gmail.com>
2025-01-12 21:26:52 +0200

    Exercise 08. Process Odd Positions

--- 03. Arrays and Nested Arrays - Lab/exercises.js
+++ 03. Arrays and Nested Arrays - Lab/exercises.js
@@ -179,3 +179,26 @@ console.log(pieceOfPie(['Apple Crisp',
 ));
 
 
+// 08. Process Odd Positions
+
+// You are given an array of numbers. Write a JS function that 
+// returns the elements at odd positions from the array, doubled and in reverse order.
+// The input comes as an array of number elements.
+// The output is the return on the console on a single line, separated by space.
+
+function oddPositions(array){
+    let resultArray = [];
+
+    for (let i = 0; i < array.length; i++) {
+        if(i % 2 !== 0){
+            resultArray.push(array[i]*2)
+        }
+    }
+
+    return resultArray.reverse();
+
+}
+
+console.log(oddPositions([10, 15, 20, 25]));
+console.log(oddPositions([3, 0, 10, 4, 7, 3]));
+

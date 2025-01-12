55255b4bb8f27f0691a6cb5bab500504277e362c
I-M-Marinov <lcfrrr@gmail.com>
2025-01-12 22:01:35 +0200

    Exercise 03. Add and Remove Elements

--- 04. Arrays and Nested Arrays - Exercise/exercises.js
+++ 04. Arrays and Nested Arrays - Exercise/exercises.js
@@ -67,4 +67,51 @@ console.log(printNthOfArray(['1',
     6    
     ));
 
+    // 03. Add and Remove Elements
+
+    // Write a JS function that adds and removes numbers to/from an array. 
+    // You will receive a command which can either be "add" or "remove". 
+    // The initial number is 1. 
+    // Upon receiving an "add" command you should add the current number to your array. 
+    // Upon receiving the "remove" command you should remove the last entered number, currently existent in the array.
+    // Each input command should increase that number, regardless of what it is.
+    // The input comes as an array of strings. Each element holds a command. 
+    // The output is the element of the array, each printed on a new line. 
+    // In case of an empty array, just print "Empty".
+    function addRemoveElements(array) {
+        let initialNumber = 1; 
+        let resultArray = []; 
+
+        for (const command of array) {
+            if (command === 'add') {
+                resultArray.push(initialNumber); 
+            } else if (command === 'remove') {
+                resultArray.pop(); 
+            }
+            initialNumber++; 
+        }
+    
+        if (resultArray.length === 0) {
+            console.log("Empty");
+        } else {
+            console.log(resultArray.join('\n'));
+        }
+    }
+
+    addRemoveElements(['add', 
+        'add', 
+        'add', 
+        'add']
+        );
+    addRemoveElements(['add', 
+        'add', 
+        'remove', 
+        'add', 
+        'add']        
+        );
+    addRemoveElements(['remove', 
+        'remove', 
+        'remove']
+        );
+
 

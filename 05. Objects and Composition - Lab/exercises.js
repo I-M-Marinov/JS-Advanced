c021254a7a08d82130ba5ca5b3fb8394f84225cb
Ivan Marinov <134225981+I-M-Marinov@users.noreply.github.com>
2025-01-14 18:00:57 +0200

    Exercise 05.Assembly Line

--- 05. Objects and Composition - Lab/exercises.js
+++ 05. Objects and Composition - Lab/exercises.js
@@ -190,3 +190,72 @@ const library = {
   ];
   const products = factory(library, orders);
   console.log(products);
+
+  // 05.Assembly Line
+
+  function createAssemblyLine() {
+    return {
+        hasClima(carObject) {
+            carObject.temp = 21;
+            carObject.tempSettings = 21;
+            carObject.adjustTemp = function () {
+                if (this.temp < this.tempSettings) {
+                    this.temp++;
+                } else if (this.temp > this.tempSettings) {
+                    this.temp--;
+                }
+            };
+        },
+
+        hasAudio(carObject) {
+            carObject.currentTrack = null;
+            carObject.nowPlaying = function () {
+                if (this.currentTrack) {
+                    console.log(`Now playing '${this.currentTrack.name}' by ${this.currentTrack.artist}`);
+                }
+            };
+        },
+
+        hasParktronic(carObject) {
+            carObject.checkDistance = function (distance) {
+                if (distance < 0.1) {
+                    console.log("Beep! Beep! Beep!");
+                } else if (distance >= 0.1 && distance < 0.25) {
+                    console.log("Beep! Beep!");
+                } else if (distance >= 0.25 && distance < 0.5) {
+                    console.log("Beep!");
+                } else {
+                    console.log(""); 
+                }
+            };
+        },
+    };
+}
+
+
+
+const assemblyLine = createAssemblyLine();
+const myCar = {
+    make: 'Toyota',
+    model: 'Avensis'
+};
+
+assemblyLine.hasClima(myCar);
+console.log(myCar.temp);
+myCar.tempSettings = 18;
+myCar.adjustTemp();
+console.log(myCar.temp);
+
+assemblyLine.hasAudio(myCar);
+myCar.currentTrack = {
+    name: 'Never Gonna Give You Up',
+    artist: 'Rick Astley'
+};
+myCar.nowPlaying();
+
+assemblyLine.hasParktronic(myCar);
+myCar.checkDistance(0.4);
+myCar.checkDistance(0.2);
+
+console.log(JSON.stringify(myCar));
+

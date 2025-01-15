b5d2aaa63f5d920c8e24f0467ffb9e9e2106b8b9
I-M-Marinov <lcfrrr@gmail.com>
2025-01-15 18:28:41 +0200

    Exercise 6. From JSON to HTML Table

--- 05. Objects and Composition - Lab/exercises.js
+++ 05. Objects and Composition - Lab/exercises.js
@@ -259,3 +259,70 @@ myCar.checkDistance(0.2);
 
 console.log(JSON.stringify(myCar));
 
+
+// 6. From JSON to HTML Table
+
+// You’re tasked with creating an HTML table of students and their scores. 
+// You will receive a single string representing an array of objects, 
+// the table’s headings should be equal to the object’s keys, while each object’s values should be a new entry in the table. 
+// Any text values in an object should be escaped, to avoid introducing dangerous code into the HTML.
+
+// Input
+// The input comes with a single string argument (the array of objects).
+
+// Output
+// The output should be printed on the console – for each entry row in the input print the object representing it.
+// Note: Objects’ keys will always be the same. Check more information for the HTML Entity here.
+
+// HTML
+// You are provided with an HTML file to test your table in the browser.
+
+
+function fromJSONToHTML(input) {
+    const array = JSON.parse(input);
+
+    const headers = Object.keys(array[0]);
+
+    const headerRow = `  <tr>${headers.map(h => `<th>${escapeHtml(h.trim())}</th>`).join('')}</tr>`;
+    const rows = array
+        .map(entry =>
+            `  <tr>${headers.map(h => `<td>${escapeHtml(entry[h])}</td>`).join('')}</tr>`
+        )
+        .join('\n');
+
+        let result = `<table>\n${headerRow}\n${rows}\n</table>`;
+
+    return result.trim();
+
+    function escapeHtml(value) {
+        return value
+            .toString()
+            .replace(/&/g, '&amp;')
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;')
+            .replace(/"/g, '&quot;')
+            .replace(/'/g, '&#39;');
+    };
+}
+
+
+
+
+console.log(fromJSONToHTML(`[{"Name":"Stamat",
+    "Score":5.5},
+    {"Name":"Rumen",
+    "Score":6}]`
+  ));
+
+
+console.log(fromJSONToHTML(`[{"Name":"Pesho",
+    "Score":4,
+    " Grade":8},
+  {"Name":"Gosho",
+   "Score":5,
+   " Grade":8},
+  {"Name":"Angel",
+   "Score":5.50,
+   " Grade":10}]`
+  ));
+

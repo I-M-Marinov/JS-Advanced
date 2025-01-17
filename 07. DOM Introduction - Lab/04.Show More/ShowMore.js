7a1e9c3aa70bc30ed489733edcfa3e16e83e5d7e
I-M-Marinov <lcfrrr@gmail.com>
2025-01-17 12:18:41 +0200

    Exercise 04. Show More

---
+++ 07. DOM Introduction - Lab/04.Show More/ShowMore.js
@@ -0,0 +1,8 @@
+function showText() {
+    
+    const moreElement = document.getElementById('more');
+    const textElement = document.getElementById('text');
+
+    moreElement.style.display = 'none';
+    textElement.style.display = 'inline';
+}

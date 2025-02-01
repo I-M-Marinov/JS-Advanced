039f0729731139b5ecc2688ccacbf412ac3b5de0
I-M-Marinov <lcfrrr@gmail.com>
2025-02-01 21:27:19 +0200

    Exercise 06. Dynamic Validation

---
+++ 14. Unit Testing and Error Handling - Exercise/06.Dynamic-Validation/app.js
@@ -0,0 +1,13 @@
+function validate() {
+    const regEx = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/gm;
+    const inputElement = document.getElementById('email');
+
+    inputElement.addEventListener('change', () => {
+
+        if(!regEx.test(inputElement.value)) {
+            inputElement.classList.add('error')
+        } else {
+            inputElement.removeAttribute('class')
+        }
+    })
+}

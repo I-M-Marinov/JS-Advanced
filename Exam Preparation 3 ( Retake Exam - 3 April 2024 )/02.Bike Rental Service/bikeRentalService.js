207eb9c64bf8012fd98c6533d39415be4d6877c2
I-M-Marinov <lcfrrr@gmail.com>
2025-02-10 01:00:16 +0200

    Fixed Exercise 02.Bike Rental Service

--- Exam Preparation 3 ( Retake Exam - 3 April 2024 )/02.Bike Rental Service/bikeRentalService.js
+++ Exam Preparation 3 ( Retake Exam - 3 April 2024 )/02.Bike Rental Service/bikeRentalService.js
@@ -5,47 +5,42 @@ class BikeRentalService {
         this.availableBikes = [];
     }
 
-    addBikes(bikes){
-
-        class Bike{
-            constructor(brand,quantity,price){
+    addBikes(bikes) {
+        class Bike {
+            constructor(brand, quantity, price) {
                 this.brand = brand;
                 this.quantity = quantity;
                 this.price = price;
             }
         }
-
-        let result = '';
-
+    
+        let newlyAddedBrands = new Set(); 
+    
         for (const line of bikes) {
             let [brand, quantity, price] = line.split('-');
-            quantity = parseInt(quantity);
-            price = parseFloat(price);
-            let newBike = new Bike(brand,quantity,price);
-
-            const existingBike = this.availableBikes.find(b => {
-                return b.brand === brand 
-            })
-
-            if(existingBike){
+            price = Number(price);
+            quantity = Number(quantity);
+            
+            let existingBike = this.availableBikes.find(b => b.brand === brand);
+    
+            if (existingBike) {
                 existingBike.quantity += quantity;
-                if(existingBike.price < price){
+                if (existingBike.price < price) {
                     existingBike.price = price;
                 }
-
-            } else{
-                this.availableBikes.push(newBike);         
+            } else {
+                this.availableBikes.push(new Bike(brand, quantity, price));
+                newlyAddedBrands.add(brand); 
             }
-
-            result = this.availableBikes.map(b => b.brand).join(", ");
         }
-
-        return `Successfully added ${result}`;
+    
+        return `Successfully added ${[...newlyAddedBrands].join(", ")}`;
     }
-
+    
     rentBikes(selectedBikes) {
         let totalPrice = 0;
-    
+        let invalidBrandOrQuantity = false;
+
         for (const line of selectedBikes) {
             let [brand, quantity] = line.split('-');
             quantity = parseInt(quantity); 
@@ -53,11 +48,15 @@ class BikeRentalService {
             let bike = this.availableBikes.find(b => b.brand === brand);
     
             if (!bike || bike.quantity < quantity) {
-                return `Some of the bikes are unavailable or low on quantity in the bike rental service.`;
+                invalidBrandOrQuantity = true;
+            } else{
+                totalPrice += quantity * parseFloat(bike.price);
+                bike.quantity -= quantity; 
             }
-    
-            totalPrice += quantity * bike.price;
-            bike.quantity -= quantity; 
+        }
+
+        if(invalidBrandOrQuantity){
+            return `Some of the bikes are unavailable or low on quantity in the bike rental service.`;
         }
     
         return `Enjoy your ride! You must pay the following amount $${totalPrice.toFixed(2)}.`;
@@ -73,7 +72,7 @@ class BikeRentalService {
             let bike = this.availableBikes.find(b => b.brand === brand);
     
             if (bike) {
-                bike.quantity += quantity; 
+                bike.quantity += quantity;  
             } else {
                 hasInvalidBike = true; 
             }
@@ -84,35 +83,47 @@ class BikeRentalService {
             : "Thank you for returning!";
     }
     
+
+    revision() {
+        let result = [];
+        result.push(`Available bikes:`);
     
-
-    revision(){
-        
-        let sortedBikes = this.availableBikes.sort((a, b) => parseFloat(a.price) - parseFloat(b.price));
-        
-        const bikeInfo = sortedBikes.map(b => `${b.brand} quantity:${b.quantity} price:$${b.price}`);
-
-        return `Available bikes:\n${bikeInfo.join('\n')}\nThe name of the bike rental service is ${this.name}, and the location is ${this.location}.`
-        
-    }
-
-
+        const sortedBikes = this.availableBikes.sort((a, b) => a.price - b.price);
+    
+        for (let bike of sortedBikes) {
+          result.push(`${bike.brand} quantity:${bike.quantity} price:$${bike.price}`);
+        }
+        result.push(
+          `The name of the bike rental service is ${this.name}, and the location is ${this.location}.`
+        );
+        return result.join(`\n`);
+      }
 }
 
 
 // const rentalService = new BikeRentalService("MyBikes", "CityCenter");
 // console.log(rentalService.addBikes(["Mountain Bike-5-150", "City Bike-10-100", "Electric Bike-3-200", "Electric Bike-8-400"]));
 
-const rentalService = new BikeRentalService("MyBikes", "CityCenter");
-console.log(rentalService.addBikes(["Mountain Bike-5-150", "City Bike-10-100", "Electric Bike-3-200", "Electric Bike-8-400"]));
-console.log(rentalService.rentBikes(["Mountain Bike-2", "City Bike-5"]));
 
 // const rentalService = new BikeRentalService("MyBikes", "CityCenter");
+// console.log(rentalService.addBikes(["Mountain Bike-5-150", "City Bike-10-100", "Electric Bike-3-200", "Electric Bike-8-400"]));
+// console.log(rentalService.rentBikes(["Mountain Bike-2", "City Bike-5"]));
 
+
+// const rentalService = new BikeRentalService("MyBikes", "CityCenter");
 // console.log(rentalService.addBikes(["Mountain Bike-5-150", "City Bike-10-100", "Electric Bike-3-200", "Electric Bike-8-400"]));
 // console.log(rentalService.rentBikes(["Mountain Bike-2", "City Bike-5", "Stunt Bike-5"]));
 // console.log(rentalService.returnBikes(["Mountain Bike-1", "City Bike-3", "Race Bike-5"]));
 // console.log(rentalService.revision());
+
+
+const rentalService = new BikeRentalService("MyBikes", "CityCenter");
+
+console.log(rentalService.addBikes(["Mountain Bike-5-150", "City Bike-10-100", "Electric Bike-3-200", "Electric Bike-8-400"]));
+console.log(rentalService.rentBikes(["Mountain Bike-5", "City Bike-5"]));
+console.log(rentalService.returnBikes(["Mountain Bike-1", "City Bike-3"]));
+console.log(rentalService.revision());
+
 
 
 
